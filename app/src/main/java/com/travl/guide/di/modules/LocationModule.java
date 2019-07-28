a921739b2ff72ba448ec55d004da5620640d68c0
ro-mak <eee127@yandex.ru>
2019-07-28 18:51:02 +0300

    Travl-feature/StartPageRefactoring: -androidx migration start attempt

--- app/src/main/java/com/travl/guide/di/modules/LocationModule.java
+++ app/src/main/java/com/travl/guide/di/modules/LocationModule.java
@@ -2,7 +2,6 @@ package com.travl.guide.di.modules;
 
 import com.travl.guide.mvp.model.location.LocationRequester;
 import com.travl.guide.mvp.model.location.StartPageLocationRequester;
-import com.travl.guide.ui.App;
 
 import dagger.Module;
 import dagger.Provides;
@@ -10,7 +9,7 @@ import dagger.Provides;
 @Module
 public class LocationModule {
     @Provides
-    public LocationRequester getLocationRequester(App instance) {
-        return new StartPageLocationRequester(instance);
+    public LocationRequester getLocationRequester() {
+        return new StartPageLocationRequester();
     }
 }
--- app/src/main/java/com/travl/guide/mvp/model/location/StartPageLocationRequester.java
+++ app/src/main/java/com/travl/guide/mvp/model/location/StartPageLocationRequester.java
@@ -4,12 +4,15 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.travl.guide.mvp.model.network.CoordinatesRequest;
 import com.travl.guide.ui.App;
 
@@ -17,14 +20,13 @@ import io.reactivex.Observable;
 import io.reactivex.annotations.Nullable;
 import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
-import timber.log.Timber;
 
 import static com.travl.guide.util.UtilVariables.FINE_LOCATION_PERMISSION;
 import static com.travl.guide.util.UtilVariables.LOCATION_PERMISSIONS_REQUEST_CODE;
 
 public class StartPageLocationRequester implements LocationRequester {
-    private LocationListener mListener;
-    private LocationManager locationManager;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
     private PublishSubject<CoordinatesRequest> coordinatesRequestPublishSubject = PublishSubject.create();
     private double[] coordinates;
 
@@ -43,31 +45,18 @@ public class StartPageLocationRequester implements LocationRequester {
         return coordinatesRequestPublishSubject.subscribeOn(Schedulers.io());
     }
 
-    public StartPageLocationRequester(App instance) {
-        locationManager = (LocationManager) instance.getSystemService(Context.LOCATION_SERVICE);
-    }
-
     public void initLocationListener(LocationPresenter presenter) {
-        presenter.observeUserCoordinates();
-        mListener = new LocationListener() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationCallback = new LocationCallback(){
             @Override
-            public void onLocationChanged(Location location) {
-                Timber.e("onLocationChanged");
-                if (location != null) {
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null) {
+                    Location location = locationResult.getLastLocation();
                     setCoordinates(new double[]{location.getLatitude(), location.getLongitude()});
                 }
-            }
-
-            @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-            }
-
-            @Override
-            public void onProviderEnabled(String s) {
-            }
-
-            @Override
-            public void onProviderDisabled(String s) {
             }
         };
     }
@@ -101,30 +90,7 @@ public class StartPageLocationRequester implements LocationRequester {
 
     @SuppressLint("MissingPermission")
     private void requestLocation() {
-        int millisInSecond = 1000;
-        int minutes = 1;
-        int secondsInMinutes = 60;
-        int amplifier = 10;
-        int meters = 10;
-        String networkProvider = LocationManager.NETWORK_PROVIDER;
-        String gpsProvider = LocationManager.GPS_PROVIDER;
-        String passiveProvider = LocationManager.PASSIVE_PROVIDER;
-        if (locationManager.isProviderEnabled(passiveProvider)) {
-            Timber.e("passive");
-            locationManager.requestLocationUpdates(passiveProvider,
-                    minutes * secondsInMinutes * millisInSecond / amplifier, meters, mListener);
-        }
-        if (locationManager.isProviderEnabled(networkProvider)) {
-            Timber.e("networkProvider");
-            locationManager.requestLocationUpdates(networkProvider,
-                    minutes * secondsInMinutes * millisInSecond / amplifier, meters, mListener);
-        }
-
-        if (locationManager.isProviderEnabled(gpsProvider)) {
-            Timber.e("gpsProvider");
-            locationManager.requestLocationUpdates(gpsProvider,
-                    minutes * secondsInMinutes * millisInSecond / amplifier, meters, mListener);
-        }
-
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(App.getInstance());
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,null);
     }
 }
