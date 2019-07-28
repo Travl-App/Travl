c68dd3c9abe1948a53f1b1ef508ebff33a933d96
ro-mak <eee127@yandex.ru>
2019-07-28 08:47:42 +0300

    -started to refactor Start page

--- app/src/main/java/com/travl/guide/mvp/view/start/page/StartPageView.java
+++ app/src/main/java/com/travl/guide/mvp/view/start/page/StartPageView.java
@@ -4,38 +4,15 @@ import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.AddToEndSingleStrategy;
 import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
-import com.travl.guide.mvp.model.api.city.content.CitiesList;
 import com.travl.guide.mvp.model.api.city.content.City;
-import com.travl.guide.mvp.model.api.city.content.CityContent;
+import com.travl.guide.mvp.model.location.LocationReceiver;
 
-import java.util.ArrayList;
+import java.util.List;
 
 @StateStrategyType(value = AddToEndSingleStrategy.class)
-public interface StartPageView extends MvpView {
-
-
-    @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void setCityObjectList(CitiesList cityObjectList);
-
-    @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void setCityContentByCoordinates(CityContent cityContent);
-
-    @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void setCityContentByLinkId(CityContent cityContent);
-
-    @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void setCurrentCity(CityContent cityContent);
+public interface StartPageView extends MvpView, LocationReceiver {
 
     void requestLocationPermissions();
-
-    @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void setCityStringNames(ArrayList<String> citiesListToCitiesNameList);
-
-    @StateStrategyType(value = AddToEndSingleStrategy.class)
-    void onSpinnerItemClick(String selectedCity);
-
-    @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void addToCityList(City city, boolean isUserCity);
 
     @StateStrategyType(value = OneExecutionStateStrategy.class)
     void setSpinnerPositionSelected(int position);
@@ -47,11 +24,19 @@ public interface StartPageView extends MvpView {
     void removePlaceIfIsAdded(String placeName);
 
     @StateStrategyType(value = OneExecutionStateStrategy.class)
-    void setCityArticles();
+    void setCityArticles(City currentCity);
 
     void onLocationPermissionRequestGranted();
 
-    void transformCityObjectsToCityStrings();
+    void addNamesToCitySpinner(List<String> names);
 
-    void addNamesToCitySpinner();
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
+    void initCityArticlesFragment();
+
+    @StateStrategyType(value = OneExecutionStateStrategy.class)
+    void initTravlZineFragment();
+
+    void initCitySpinner();
+
+    void initMoveToNavigator();
 }
